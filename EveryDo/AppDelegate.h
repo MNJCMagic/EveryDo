cb56bf0c57ffcebc202ee4a2e8b38b2ef8e8dd58
Mike Cameron <mike@mikecameron.tv>
2018-04-24 10:08:00 -0400

    Initial Commit

---
+++ EveryDo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EveryDo
+//
+//  Created by Mike Cameron on 2018-04-24.
+//  Copyright © 2018 Mike Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
